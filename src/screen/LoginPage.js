53f4b7fc8096a846f9bf2f9d2f282335988efd3e
Omer-Ozc <o.ozctn@gmail.com>
2020-08-31 11:54:52 +0300

    Sign And Login Finish

--- src/screen/LoginPage.js
+++ src/screen/LoginPage.js
@@ -8,46 +8,52 @@ export default class LoginPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            uName:'',
-            uPassword:'',
-            name : '',
-            password : '',
-        };
+            uName: '',
+            uPassword: '',
+            userDatas:[]
+        }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getItemData()
     }
 
     getItemData = async () => {
         console.log('calisti')
-      try {
-        const Accaount = await AsyncStorage.getItem('Accounts');
-        if (Accaount !== null) {
+        try {
+            const Accaount = await AsyncStorage.getItem('Accounts');
+            let userData = []
+            if (Accaount !== null) {
+                userData.push(JSON.parse(Accaount))
+                this.setState({userDatas:userData})
+            }
+            console.log('UserData', userData)
 
-            const Accm = JSON.parse(Accaount)
-
-          this.setState({name:Accm.name})
-          this.setState({password:Accm.password})
-          console.log({Accaount})
+        } catch (error) {
+            alert(error)
         }
-      } catch (error) {
-        alert(error)
-      }
+
+
     };
 
-    touchAblePressFunction(){
+    touchAblePressFunction() {
         this.props.navigation.navigate('SignUpPage')
     }
 
-    LoginFunction(){
-        if(this.state.uName == this.state.name && this.state.uPassword == this.state.password){
+    LoginFunction() {
+        let isOk = true
+
+        for(let i = 0 ; i< this.state.userDatas[0].length ; i++){
+           // console.log(this.state.userDatas[0][i]['name'])
+
+           if(this.state.uName == this.state.userDatas[0][i]['name'] && this.state.uPassword == this.state.userDatas[0][i]['password']){
             this.props.navigation.navigate('Successfull')
+            isOk=false
+           }
         }
-        else{
-            alert('Wrong ID or Password')
+        if(isOk){
+            alert('Wrong ID Or Password')
         }
-        
 
     }
 
@@ -64,16 +70,16 @@ export default class LoginPage extends Component {
                     onChangeText={(text) => this.setState({ uPassword: text })}
                     value={this.state.uPassword} />
 
-                <Button style = {styles.buttonStyle}
+                <Button style={styles.buttonStyle}
                     title="Login"
                     color='red'
-                    onPress = {() => this.LoginFunction() }
+                    onPress={() => this.LoginFunction()}
                 />
 
-                <TouchableOpacity style = {styles.touchAbleOpacityStyle}
-                onPress = {()=>this.props.navigation.navigate('SignUpPage')}>
+                <TouchableOpacity style={styles.touchAbleOpacityStyle}
+                    onPress={() => this.props.navigation.navigate('SignUpPage')}>
 
-                    <Text style={{color:'red'}}>Sign Up</Text>
+                    <Text style={{ color: 'red' }}>Sign Up</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -97,15 +103,15 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         alignSelf: 'center',
         textAlign: 'center',
-        marginBottom:5
+        marginBottom: 5
     },
 
-    buttonStyle : {
-        borderRadius:50,
-        marginBottom:5
+    buttonStyle: {
+        borderRadius: 50,
+        marginBottom: 5
     },
 
-    touchAbleOpacityStyle :{
+    touchAbleOpacityStyle: {
 
 
     }
--- src/screen/SignUpPage.js
+++ src/screen/SignUpPage.js
@@ -4,113 +4,116 @@ import { TextInput } from 'react-native-gesture-handler';
 import { AsyncStorage } from 'react-native';
 
 export default class SignUpPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-            name: '',
-            password : '',
-        };
-    }
-
-    setItemData = async (acc) => {
-        console.log(acc.name)
-
-        try {
-          await AsyncStorage.setItem(
-            'Accounts',
-            JSON.stringify(acc)
-            
-          );
-        console.log('yazdıdrd')
-
-        } catch (error) {
-          alert(error)
-        }
-      };
-
-      componentDidMount(){
-          this.getItemData()
-      }
-
-
-      getItemData = async () => {
-        console.log('calisti')
-      try {
-        const Accaount = await AsyncStorage.getItem('Accounts');
-        if (Accaount !== null) {
-
-            const Accm = JSON.parse(Accaount)
-
-          this.setState({name:Accm.name})
-          this.setState({password:Accm.password})
-        }
-      } catch (error) {
-        alert(error)
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      password: '',
     };
+  }
+
+  setItemData = async (acc) => {
+    let userData;
+    const Accaount = await AsyncStorage.getItem('Accounts');
+    console.log('ilk Giriş Accaount',Accaount)
+    if (Accaount !== null) {
+      //userData = [...JSON.parse(Accaount), acc]
+      console.log('içeride ',Accaount)
+      userData=JSON.parse(Accaount)
       
+    }
+    else {
+      console.log('else')
+      userData = []
+    }
+    console.log(userData)
 
 
-    render() {
+    try {
+      await AsyncStorage.setItem(
+        'Accounts',
+        JSON.stringify([...userData, acc])
+      );
 
-       
-        return (
-            <View style={styles.container}>
+    } catch (error) {
+      alert(error)
+    }
+  };
 
-                
+  async removeItemValue(key) {
+    try {
+      await AsyncStorage.removeItem('Accounts');
+      return true;
+    }
+    catch (exception) {
+      return false;
+    }
+  }
+
+
+
+
+
+  render() {
+
+
+    return (
+      <View style={styles.container}>
+
+
 
         <Text>{this.state.name}</Text>
 
-                <TextInput style={styles.textInputStyle}
-                    placeholder="Enter Your ID"
-                    onChangeText={(text) => this.setState({ name: text })}
-                    value={this.state.name}
-                />
+        <TextInput style={styles.textInputStyle}
+          placeholder="Enter Your ID"
+          onChangeText={(text) => this.setState({ name: text })}
+          value={this.state.name}
+        />
 
-                <TextInput style={styles.textInputStyle}
-                    placeholder="Enter Your Password"
-                    onChangeText={(text) => this.setState({ password: text })}
-                    value={this.state.password}
-                />
+        <TextInput style={styles.textInputStyle}
+          placeholder="Enter Your Password"
+          onChangeText={(text) => this.setState({ password: text })}
+          value={this.state.password}
+        />
 
-                <Button style={styles.buttonStlye}
-                    title="Sign Up"
-                    color='red'
-                    onPress={() => this.setItemData(this.state)}
-                />
+        <Button style={styles.buttonStlye}
+          title="Sign Up"
+          color='red'
+          onPress={() => this.setItemData(this.state)}
+        />
 
-                <Button
-                title = "Bas"
-                onPress={() => this.getItemData()}
-                />
-            </View>
-        );
-    }
+        <Button
+          title="Sil"
+          onPress={() => this.removeItemValue()}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#D3D3D3',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    textInputStyle: {
-        backgroundColor: 'white',
-        width: '75%',
-        borderColor: '#D3D3D3',
-        alignItems: 'center',
-        borderWidth: 2,
-        borderRadius: 50,
-        alignSelf: 'center',
-        textAlign: 'center',
-        marginBottom: 5
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#D3D3D3',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInputStyle: {
+    backgroundColor: 'white',
+    width: '75%',
+    borderColor: '#D3D3D3',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderRadius: 50,
+    alignSelf: 'center',
+    textAlign: 'center',
+    marginBottom: 5
+  },
 
-    buttonStlye: {
-        color: 'red',
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  buttonStlye: {
+    color: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 })
